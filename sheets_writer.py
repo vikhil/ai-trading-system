8988abbf51fab85122d3485d1b6f8843248b398a
vikhil <vikhil@gmail.com>
2026-05-31 02:36:13 +0530

    Update sheets_writer.py

--- sheets_writer.py
+++ sheets_writer.py
@@ -3,7 +3,12 @@ import time
 def safe_update(ws, data):
     try:
         ws.clear()
+
+        print(f"[SHEETS DEBUG] Writing to {ws.title}")
+        print(f"[SHEETS DEBUG] Rows={len(data)}")
+
         ws.update("A1", data)
+
         print(f"[SHEETS OK] Updated {ws.title} rows={len(data)}")
 
     except Exception as e:
