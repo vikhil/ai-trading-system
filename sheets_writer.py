d1affb025753a2c8d1ea5d1eee46df02de766b8e
vikhil <vikhil@gmail.com>
2026-05-31 10:26:31 +0530

    Update sheets_writer.py

--- sheets_writer.py
+++ sheets_writer.py
@@ -7,7 +7,6 @@ def safe_update(ws, data):
         print(f"[SHEETS DEBUG] Writing to {ws.title}")
         print(f"[SHEETS DEBUG] Rows={len(data)}")
 
-        #ws.update("A1", data)
         ws.update(
             range_name="A1",
             values=data
