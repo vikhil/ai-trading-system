f6351149aa6749db21ee45fbbc859f28cd5d55e4
vikhil <vikhil@gmail.com>
2026-05-31 09:59:29 +0530

    Update sheets_writer.py

--- sheets_writer.py
+++ sheets_writer.py
@@ -7,10 +7,18 @@ def safe_update(ws, data):
         print(f"[SHEETS DEBUG] Writing to {ws.title}")
         print(f"[SHEETS DEBUG] Rows={len(data)}")
 
-        ws.update("A1", data)
-
+        #ws.update("A1", data)
+        ws.update(
+            range_name="A1",
+            values=data
+        )
         print(f"[SHEETS OK] Updated {ws.title} rows={len(data)}")
 
+        print(
+            "VERIFY:",
+            ws.acell("A1").value
+        )
+        
     except Exception as e:
         print(f"[SHEETS ERROR] {ws.title} -> {e}")
         raise
