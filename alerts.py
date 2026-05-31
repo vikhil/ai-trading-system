9e031d3afc820e457bc1c0607fcce10f8b376a4f
vikhil <vikhil@gmail.com>
2026-06-01 01:08:12 +0530

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -3,24 +3,26 @@ import requests
 
 def send_telegram(message):
 
-   TOKEN = os.getenv("TG_TOKEN")
-    CHAT_ID = os.getenv("TG_CHAT_ID")
-    
-    if not TOKEN or not CHAT_ID:
-        print("Telegram not configured")
+    token = os.getenv("TG_TOKEN")
+    chat_id = os.getenv("TG_CHAT_ID")
+
+    if not token:
+        print("TELEGRAM ERROR: TG_TOKEN missing")
+        return
+
+    if not chat_id:
+        print("TELEGRAM ERROR: TG_CHAT_ID missing")
         return
 
     url = f"https://api.telegram.org/bot{token}/sendMessage"
 
-    try:
-        requests.post(
-            url,
-            data={
-                "chat_id": chat_id,
-                "text": message
-            },
-            timeout=10
-        )
+    response = requests.post(
+        url,
+        json={
+            "chat_id": chat_id,
+            "text": message
+        }
+    )
 
-    except Exception as e:
-        print("Telegram Error:", e)
+    print("Telegram Status:", response.status_code)
+    print("Telegram Response:", response.text)
