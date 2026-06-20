586d0ebeefdad52a38fd86e10a9011a57f9f4ceb
vikhil <vikhil@gmail.com>
2026-06-20 07:27:59 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -4,41 +4,75 @@ from io import StringIO
 import yfinance as yf
 
 def fetch_nse_index_stocks(url):
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers)
+
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(
+        url,
+        headers=headers
+    )
 
     if response.status_code != 200:
-        raise Exception(f"NSE fetch failed: {response.status_code}")
+        raise Exception(
+            f"NSE fetch failed: {response.status_code}"
+        )
 
     df = pd.read_csv(StringIO(response.text))
-    return [s + ".NS" for s in df["Symbol"].tolist()]
 
-def load_universe():
-    nifty50_url = "https://archives.nseindia.com/content/indices/ind_nifty50list.csv"
-    niftynext50_url = "https://archives.nseindia.com/content/indices/ind_niftynext50list.csv"
-    #nifty500_url = "https://nsearchives.nseindia.com/content/indices/ind_nifty500list.csv"
-    
-    stocks = list(set(
-        fetch_nse_index_stocks(nifty50_url) +
-        fetch_nse_index_stocks(niftynext50_url) 
-        #+
-        #fetch_nse_index_stocks(nifty500_url)
-    ))
+    stocks = []
 
-    # CLEAN STOCKS
-    stocks = [s.strip().upper() for s in stocks]
+    for _, row in df.iterrows():
 
-    # REMOVE INVALID / DUMMY SYMBOLS
-    stocks = [
-        s for s in stocks
-        if s.endswith(".NS")
-        and "DUMMY" not in s
-    ]
+        stocks.append({
 
-    # REMOVE DUPLICATES WHILE KEEPING ORDER
-    stocks = list(dict.fromkeys(stocks))
+            "Ticker": row["Symbol"].strip().upper() + ".NS",
+
+            "Sector": row.get(
+                "Industry",
+                "UNKNOWN"
+            )
+
+        })
 
     return stocks
+
+def load_universe():
+
+    nifty50_url = "https://archives.nseindia.com/content/indices/ind_nifty50list.csv"
+    niftynext50_url = "https://archives.nseindia.com/content/indices/ind_niftynext50list.csv"
+    NIFTY500_URL = "https://archives.nseindia.com/content/indices/ind_nifty500list.csv"
+
+    universe = []
+
+    universe = (
+        fetch_nse_index_stocks(nifty50_url)
+        +
+        fetch_nse_index_stocks(niftynext50_url)
+        +
+        fetch_nse_index_stocks(NIFTY500_url)
+    )
+
+    cleaned = []
+    seen = set()
+
+    for stock in universe:
+
+        ticker = stock["Ticker"].strip().upper()
+
+        if (
+            ticker.endswith(".NS")
+            and "DUMMY" not in ticker
+            and ticker not in seen
+        ):
+
+            stock["Ticker"] = ticker
+
+            cleaned.append(stock)
+            seen.add(ticker)
+
+    return cleaned
     
 def fetch_stock_data(ticker):
     df = yf.download(
