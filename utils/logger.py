a82e3c3e45b9135cce469cc8fbf265eed358b222
vikhil <vikhil@gmail.com>
2026-06-20 11:13:51 +0530

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,18 @@
+def log_scan(msg):
+    print(f"[SCAN] {msg}")
+
+
+def log_signal(msg):
+    print(f"[SIGNAL] {msg}")
+
+
+def log_risk(msg):
+    print(f"[RISK] {msg}")
+
+
+def log_exec(msg):
+    print(f"[EXECUTION] {msg}")
+
+
+def log_error(msg):
+    print(f"[ERROR] {msg}")
